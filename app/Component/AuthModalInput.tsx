667e882c906cafc5174b68dc257053ec86a15b8f
Isaac Yanez <isaac231467@gmail.com>
2023-05-11 09:34:24 -0500

    was able to redesign the login and sign up modals

--- app/Component/AuthModalInput.tsx
+++ app/Component/AuthModalInput.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import legoman from "/public/Icons/legoman.png";
+import Image from "next/image";
 
 interface Props {
   inputs: {
@@ -13,55 +15,198 @@ interface Props {
 export default function AuthModalInput({ inputs, isSignin }: Props) {
   return (
     <div>
-      <h1>{isSignin ? "Login" : "Sign up"}</h1>
       {isSignin ? (
-        <>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </>
+        <form>
+          {/* Form Wrapper */}
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <Image
+                className="mx-auto w-auto"
+                src={legoman}
+                width={100}
+                height={100}
+                alt="Lego man logo"
+              />
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Welcome back!
+              </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="text-sm">
+                      <a
+                        href="#"
+                        className="font-semibold text-yellow-500 hover:text-yellow-600 focus:outline-none focus:underline transition ease-in-out duration-200"
+                      >
+                        Forgot password?
+                      </a>
+                    </div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-700 transition-all duration-150 ease-in-out"
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Not a member?
+                <a
+                  href="#"
+                  className="font-semibold leading-6 text-yellow-500 hover:text-yellow-600 focus:outline-none focus:underline transition ease-in-out duration-200"
+                >
+                  Sign up for free now!
+                </a>
+              </p>
+            </div>
+          </div>
+        </form>
       ) : (
-        <>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Username"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          <input
-            type="text"
-            name="email"
-            id="email"
-            placeholder="Email"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Name"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-          <input
-            type="text"
-            name="password"
-            id="password"
-            placeholder="Password"
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </>
+        <form>
+          {/* Form Wrapper */}
+          <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+              <Image
+                className="mx-auto w-auto"
+                src={legoman}
+                width={100}
+                height={100}
+                alt="Lego man logo"
+              />
+              <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign up for free
+              </h2>
+            </div>
+
+            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+              <form className="space-y-6" action="#" method="POST">
+                <div>
+                  <label
+                    htmlFor="username"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Username
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      placeholder="Username"
+                      id="username"
+                      name="username"
+                      type="text"
+                      required
+                      className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Email address
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      placeholder="legobuilder@email.com"
+                      id="email"
+                      name="email"
+                      type="email"
+                      autoComplete="email"
+                      required
+                      className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <div className="flex items-center justify-between">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
+                    <div className="text-sm"></div>
+                  </div>
+                  <div className="mt-2">
+                    <input
+                      placeholder="Must be at least 8 characters"
+                      id="password"
+                      name="password"
+                      type="password"
+                      autoComplete="current-password"
+                      required
+                      className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <button
+                    type="submit"
+                    className="flex w-full justify-center rounded-md bg-yellow-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-yellow-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-700 transition-all duration-150 ease-in-out"
+                  >
+                    Sign up
+                  </button>
+                </div>
+              </form>
+
+              <p className="mt-10 text-center text-sm text-gray-500">
+                Already a member?
+                <a
+                  href="#"
+                  className="font-semibold leading-6 text-yellow-500 hover:text-yellow-600 focus:outline-none focus:underline transition ease-in-out duration-200"
+                >
+                  Sign in now!
+                </a>
+              </p>
+            </div>
+          </div>
+        </form>
       )}
     </div>
   );
