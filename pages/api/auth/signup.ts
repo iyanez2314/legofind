04a3798b1f3bc6c3ebcd3e0e1f6f314fe76e262a
Isaac Yanez <isaac231467@gmail.com>
2023-05-24 09:16:26 -0500

    found a bug not relating the users-auth will come back to this later

---
+++ pages/api/auth/signup.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import validator from "validator";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
+import { setCookie } from "cookies-next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+  }
+}
